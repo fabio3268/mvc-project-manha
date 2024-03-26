64b3d9e01a75623d750b01598ec4b45ffa189e7e
Fábio Santos <fabiosantos@ifsul.edu.br>
2024-03-26 08:51:36 -0300

    MVC - Start

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -1,6 +1,7 @@
 <?php
-const CONF_DB_HOST = "mysql";
+const CONF_DB_HOST = "localhost";
 const CONF_DB_USER = "root";
-const CONF_DB_PASS = "asdf1234";
-const CONF_DB_NAME = "db-inf-3am"; // aqui deve ser alterado para o nome do banco de dados
-const CONF_URL_TEST = "http://localhost:8080/mvc-project-manha/";
+const CONF_DB_PASS = "";
+// aqui deve ser alterado para o nome do banco de dados
+const CONF_DB_NAME = "db-inf-3am";
+const CONF_URL_TEST = "http://localhost/mvc-project-manha/";
